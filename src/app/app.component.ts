ed822bad83547405977fdfe0b0ac9dc0f4ac422b
Jose <pedrojcunha7@hotmail.com>
2025-08-22 11:05:06 +0200

    fix: login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Speak4All';
-
-  name: string = "";
-
-  onSubmit() {
-    if (!this.name) {
-      alert("Écrivez votre nom");
-      return;
-    }
-    alert("Salut" + this.name);
-    this.name = "";
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
+import { ChatComponent } from '../components/chat/chat.component';
+import { LoginComponent } from '../components/login/login.component';
 export const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: LoginComponent,
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
   }
 ];
