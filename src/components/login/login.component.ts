224d5d481e8221d3141efed58dba97ac0c333285
Jose <pedrojcunha7@hotmail.com>
2025-08-22 11:43:24 +0200

    fix: fix routes problems when trying to login

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   name: string = "";
+
+  constructor(private router: Router) { }
 
   onSubmit() {
     if (!this.name) {
       alert("Écrivez votre nom");
       return;
     }
-    alert("Salut" + this.name);
+
+    this.goTo('chat');
     this.name = "";
   }
+
+  goTo(page: string) {
+    this.router.navigate([page]);
+  }
+
 }
